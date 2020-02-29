9685598518107ecdbe0f4f2576fec4e03625d053
St4y-23 <ghost19011@gmail.com>
2020-02-29 19:31:41 +0400

    finish desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 $(function () {
 
+  // Menu
+  var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
   // FULLPAGE
   $('#fullpage').fullpage({
     autoScrolling: true,
@@ -24,6 +46,17 @@ $(function () {
         { src: '../images/fullpage/fullpage-2.jpg' },
         { src: '../images/fullpage/fullpage-3.jpg' },
     ]
+  });
+
+  // Slick Slider
+  $('.works__slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'ease-in',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slider-icon slider-prev"><use xlink:href="#arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slider-icon slider-next"><use xlink:href="#arrow"></use></svg></button>',
   });
 
 
